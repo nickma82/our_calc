4c6fe9ee1fc317b55f6a4098a726420d0ca207c2
LukasPetermann <LukasPetermann.hotmail.com>
2010-05-14 09:26:39 +0200

    Input fertig, Output noch fehlerhaft

---
+++ mycalc/sim/out_test/output_tb.vhd
@@ -0,0 +1,78 @@
+library ieee ;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+use IEEE.std_logic_arith.all;
+
+use ieee.numeric_std.all;
+
+--ENTITY
+entity output_tb is
+
+end output_tb;
+
+
+--ARCHITECTURE
+architecture behav of output_tb is
+
+--Variablen und Konstanten
+constant clock_period:time := 20 ns;
+
+signal sys_clk, sys_res_n: std_logic;
+signal vga_command	: std_logic_vector(7 downto 0);
+signal vga_command_data	: std_logic_vector(31 downto 0);
+signal vga_free		: std_logic;
+signal inp_new_data	: std_logic;
+signal inp_data		: std_logic_vector(7 downto 0);
+signal inp_del		: std_logic;
+signal pars_new_data	: std_logic;
+signal pars_data	: std_logic_vector(31 downto 0);
+
+
+begin --behave
+
+uut : entity work.input_ent
+
+port map
+	(
+		sys_clk	=> sys_clk,
+		sys_res_n => sys_res_n,
+		vga_command => vga_command,
+		vga_command_data => vga_command_data,
+		vga_free => vga_free,
+		inp_new_data => inp_new_data,
+		inp_data => inp_data,
+		inp_del => inp_del,
+		pars_new_data => pars_new_data,
+		pars_data => pars_data
+	);
+
+clkgenerator : process
+begin
+	sys_clk <= '0';
+	wait for clock_period/2;
+	sys_clk <= '1';
+	wait for clock_period/2;
+end process clkgenerator;
+
+
+process
+begin
+	sys_res_n <= '0';
+	wait for 100 ns;
+	sys_res_n <= '1';
+	wait for 100 ns;
+
+	-- BEGIN TESTS
+	-- Eingabe '0'
+	ps2_data <= x"70";
+	wait for 20 ns;
+	ps2_new_data <= '1';
+	wait for 20 ns;
+	ps2_new_data <= '0';
+	wait for 20 ns;
+	assert ps2_data(7 downto 0) = "00110000";
+	wait for 100 ns;
+
+end process;
+
+end architecture behav;
---
+++ mycalc/src/output/output.vhd
@@ -0,0 +1,102 @@
+library ieee ;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+use IEEE.std_logic_arith.all;
+
+use ieee.numeric_std.all;
+
+use work.textmode_vga_pkg.all;
+use work.textmode_vga_component_pkg.all;
+use work.textmode_vga_platform_dependent_pkg.all;
+
+-- ENTITY
+entity output_ent is
+	--generic(
+		
+	--);
+	port(
+		sys_clk		: in std_logic;	
+		sys_res_n	: in std_logic;
+		vga_command	: out std_logic_vector(7 downto 0);
+		vga_command_data	: out std_logic_vector(31 downto 0);
+		vga_free		: in std_logic;
+		inp_new_data		: in std_logic;
+		inp_data		: in std_logic_vector(7 downto 0);
+		inp_del			: in std_logic;
+		pars_new_data		: in std_logic;
+		pars_data		: in std_logic_vector(31 downto 0);
+	);
+end entity input_ent;
+
+
+-- ARCHITECTURE
+architecture output_arc of output_ent is
+
+--type
+type OUTPUT_FSM_STATE_TYPE is
+    (INIT, READY, WRITE_CHAR, WRITE_RESULT, DELETE);
+
+--constants
+constant RED : std_logic_vector(23 downto 0) := x"0000FF";
+constant GREEN : std_logic_vector(23 downto 0) := x"00FF00";
+constant BLUE : std_logic_vector(23 downto 0) := x"FF0000";
+
+--signals
+signal output_fsm_state, output_fsm_state_next : INPUT_FSM_STATE_TYPE;
+
+
+begin
+
+sync : process(sys_clk, sys_res_n)
+begin
+	if sys_res_n = '0' then
+		output_fsm_state <= INIT;
+	elsif rising_edge(sys_clk) then
+		output_fsm_state <= output_fsm_state_next;
+	end if;
+
+end process sync;
+
+next_state : process(input_fsm_state, ps2_data, ps2_new_data)
+begin
+	output_fsm_state_next <= output_fsm_state;
+	
+	case output_fsm_state is
+		when INIT => 
+			output_fsm_state_next <= READY;
+		when READY =>
+			if inp_new_data = '1' then output_fsm_state_next <= WRITE_CHAR;
+			elsif pars_new_data = '1' then output_fsm_state_next <= WRITE_RESULT;
+			elsif inp_del = '1' then output_fsm_state_next <= DELETE;
+			end if;
+		when WRITE_CHAR =>
+			if vga_free = '1' then
+				output_fsm_state_next <= READY;
+			end if;
+		when WRITE_RESULT =>
+			output_fsm_state_next <= READY;
+		when DELETE =>
+			output_fsm_state_next <= READY;
+		when others => null;
+	end case;
+end process next_state;
+
+output : process(output_fsm_state)
+begin
+	case output_fsm_state is
+		when INIT =>
+		when READY =>
+		when WRITE_CHAR =>
+			vga_command <= COMMAND_SET_CHAR;
+			vga_command_data <= WHITE & zeichen;
+		when WRITE_RESULT =>
+			
+		when DELETE =>
+		when others => null;
+	end case;
+end process output;
+
+end input_arc;
+
+
+
