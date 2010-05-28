12264074489cc8a9e99492840ebf6d53e2c2689c
LukasPetermann <LukasPetermann.hotmail.com>
2010-05-28 21:23:33 +0200

    Neuer verbesserter Ringbuffer fast fertig

---
+++ mycalc/sim/ring_test/ringbuffer2_tb.vhd
@@ -0,0 +1,141 @@
+library ieee ;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+use IEEE.std_logic_arith.all;
+
+use ieee.numeric_std.all;
+
+use work.big_pkg.all;
+
+--ENTITY
+entity ringbuffer2_tb is
+
+end ringbuffer2_tb;
+
+
+--ARCHITECTURE
+architecture behav of ringbuffer2_tb is
+
+--Variablen und Konstanten
+constant clock_period:time := 30 ns;
+
+signal sys_clk, sys_res_n: std_logic;
+signal rb_busy		: std_logic := '0';
+signal pars_new_data	: std_logic := '0';
+signal pars_data	: std_logic_vector(7 downto 0) := x"00";
+signal inp_new_data	: std_logic := '0';
+signal inp_data		: std_logic_vector(7 downto 0) := x"00";
+signal inp_del		: std_logic := '0';
+signal rb_char_newline	: std_logic := '0';
+signal rb_read_en	: std_logic := '0';
+signal rb_read_lineNr	: std_logic_vector(7 downto 0) := x"00";
+signal rb_read_data_rdy	: std_logic := '0';
+signal rb_read_data	: RAM_LINE;
+signal wr		: std_logic;
+signal address		: integer range 0 to 4090;
+signal data_in		: std_logic_vector(7 downto 0);
+signal data_out		: std_logic_vector(7 downto 0);		
+
+begin --behave
+
+ring : entity work.ringbuffer2_ent
+	port map
+	(
+		sys_clk	=> sys_clk,
+		sys_res_n => sys_res_n,
+		rb_busy => rb_busy,
+		pars_new_data => pars_new_data,
+		pars_data => pars_data,
+		inp_new_data => inp_new_data,
+		inp_data => inp_data,
+		inp_del => inp_del,
+		rb_char_newline => rb_char_newline,
+		rb_read_en => rb_read_en,
+		rb_read_lineNr => rb_read_lineNr,
+		rb_read_data_rdy => rb_read_data_rdy,
+		rb_read_data => rb_read_data,
+		wr => wr,
+		address => address,
+		data_in => data_in,
+		data_out => data_out
+	);
+
+ram : entity work.ram_ent
+	port map
+	(
+		sys_clk	=> sys_clk,
+		sys_res_n => sys_res_n,
+		wr => wr,
+		address => address,
+		data_in => data_in,
+		data_out => data_out
+	);
+
+clkgenerator : process
+begin
+	sys_clk <= '0';
+	wait for clock_period/2;
+	sys_clk <= '1';
+	wait for clock_period/2;
+end process clkgenerator;
+
+
+process
+begin
+	sys_res_n <= '0';
+	wait for 45 ns;
+	sys_res_n <= '1';
+	wait for 30 ns;
+	
+	inp_data <= x"20";
+	wait for 30 ns;
+	inp_new_data <= '1';
+	wait for 30 ns;
+	inp_new_data <= '0';
+	wait for 30 ns;
+	--assert ps2_data(7 downto 0) = "00110000";
+	wait for 30 ns;
+	
+	inp_data <= x"01";
+	wait for 30 ns;
+	inp_new_data <= '1';
+	wait for 30 ns;
+	inp_new_data <= '0';
+	wait for 30 ns;
+	--assert ps2_data(7 downto 0) = "00110000";
+	wait for 30 ns;
+	
+	--Zeichen löschen
+	inp_del <= '1';
+	wait for 30 ns;
+	inp_del <= '0';
+	wait for 30 ns;
+
+	--Zeile abfragen
+	rb_read_lineNr <= x"00";
+	wait for 30 ns;
+	rb_read_en <= '1';
+	wait for 30 ns;
+	rb_read_en <= '0';
+	wait for 30 ns;
+	--assert ps2_data(7 downto 0) = "00110000";
+	wait for 30 ns;
+
+	wait for 500 ns;
+	wait for 500 ns;
+	wait for 500 ns;
+	wait for 500 ns;
+	wait for 500 ns;
+	wait for 500 ns;
+
+	--testen ob überlauf der Zeilen funktioniert
+	--for i in 0 to 52 loop
+	--	rb_char_newline <= '1';
+	--	wait for 30 ns;
+	--	rb_char_newline <= '0';
+	--	wait for 30 ns;
+	--end loop;
+
+end process;
+
+end architecture behav;
--- mycalc/src/ringbuffer/ringbuffer_2.vhd
+++ mycalc/src/ringbuffer/ringbuffer_2.vhd
@@ -25,6 +25,9 @@ entity ringbuffer2_ent is
 		rb_read_en	: in std_logic;
 		rb_read_lineNr	: in std_logic_vector(7 downto 0);
 		rb_read_data_rdy: out std_logic;
+		rb_pars_en	: in std_logic;
+		rb_pars_lineNr	: in std_logic_vector(7 downto 0);
+		rb_pars_data_rdy: out std_logic;
 		rb_read_data	: out RAM_LINE;
 		wr		: out std_logic;
 		address		: out integer range 0 to 4090;
@@ -36,7 +39,7 @@ end entity ringbuffer2_ent;
 -- ARCHITECTURE
 architecture ringbuffer2_arc of ringbuffer2_ent is
 
-type RINGBUFFER_FSM_STATE_TYPE is (INIT, READY, WRITE_CHAR, DELETE_CHAR, LINE_REQ, LINE_RDY, NEW_LINE, WRITE_RAM, READ_RAM, WAIT_RAM);
+type RINGBUFFER_FSM_STATE_TYPE is (INIT, READY, DELETE_CHAR, LINE_REQ, LINE_RDY, NEW_LINE, WRITE_RAM, READ_RAM, WAIT_RAM, PARS_REQ, PARS_RDY);
 
 --signals
 signal ringbuffer_fsm_state, ringbuffer_fsm_state_next : RINGBUFFER_FSM_STATE_TYPE;
@@ -44,6 +47,10 @@ signal linePointer, linePointer_next 	: integer range 0 to LINE_NUMB -1;				--ze
 signal charPointer, charPointer_next	: integer range 0 to LINE_LENGTH -1;				--zeigt auf derzeitigen Char in Zeile 0
 signal byte_buffer, byte_buffer_next	: std_logic_vector(7 downto 0);					--speichert zu schreibendes Byte zwischen
 signal ram_buffer, ram_buffer_next	: std_logic_vector(7 downto 0);					--speichert RAM Byte zwischen zum lesen/schreiben
+signal lineBuffer, lineBuffer_next	: RAM_LINE;							--speichert Line zwischen um Byte für Byte rein zu speichern
+signal lineCounter, lineCounter_next	: integer range 0 to LINE_LENGTH;				--speichert beim wievielten Byte er gerade ließt
+signal lineRead, lineRead_next 		: integer range 0 to LINE_NUMB -1;				--Speichert die zu lesende Zeile (linePointer + lineNR)
+signal writeNextState, writeNextState_next	: RINGBUFFER_FSM_STATE_TYPE;
 
 begin
 
@@ -57,14 +64,20 @@ begin
 		charPointer <= charPointer_next;
 		byte_buffer <= byte_buffer_next;
 		ram_buffer <= ram_buffer_next;
+		lineBuffer <= lineBuffer_next;
+		lineCounter <= lineCounter_next;
+		lineRead <= lineRead_next;
+		writeNextState <= writeNextState_next;
 	end if;
 
 end process sync;
 
-next_state : process(ringbuffer_fsm_state, inp_new_data, pars_new_data, rb_read_en, inp_del, rb_char_newline, inp_data, pars_data, charPointer, byte_buffer)
+next_state : process(ringbuffer_fsm_state, inp_new_data, pars_new_data, rb_read_en, inp_del, rb_char_newline, inp_data, pars_data, charPointer, byte_buffer, writeNextState)
 begin
 	ringbuffer_fsm_state_next <= ringbuffer_fsm_state;
 	byte_buffer_next <= byte_buffer;
+	lineRead_next <= lineRead;
+	writeNextState_next <= writeNextState;
 	
 	case ringbuffer_fsm_state is
 		when INIT =>
@@ -72,22 +85,36 @@ begin
 		when READY =>
 			if inp_new_data = '1' and charPointer < 70 then
 				byte_buffer_next <= inp_data;
-				ringbuffer_fsm_state_next <= WRITE_CHAR;
-			elsif pars_new_data = '1' then 
+				ringbuffer_fsm_state_next <= WRITE_RAM;
+			elsif pars_new_data = '1' then
 				byte_buffer_next <= pars_data;
-				ringbuffer_fsm_state_next <= WRITE_CHAR;
-			elsif rb_read_en = '1' then ringbuffer_fsm_state_next <= LINE_REQ;
+				ringbuffer_fsm_state_next <= WRITE_RAM;
+			elsif rb_read_en = '1' then 
+				ringbuffer_fsm_state_next <= READ_RAM;
+				writeNextState_next <= LINE_REQ;
+				if linePointer + rb_read_lineNr >  LINE_NUMB then
+					lineRead_next <= conv_integer((rb_read_lineNr+linePointer)) - LINE_NUMB;
+				else
+					lineRead_next <= conv_integer((rb_read_lineNr+linePointer));
+				end if;
+			elsif rb_pars_en = '1' then 
+				ringbuffer_fsm_state_next <= READ_RAM;
+				writeNextState_next <= PARS_REQ;
+				lineRead_next <= conv_integer((linePointer));
 			elsif inp_del = '1' then ringbuffer_fsm_state_next <= DELETE_CHAR;
 			elsif rb_char_newline = '1' then ringbuffer_fsm_state_next <= NEW_LINE;
 			end if;
-      		when WRITE_CHAR =>
-			ringbuffer_fsm_state_next <= WRITE_RAM;
 		when DELETE_CHAR =>
 			ringbuffer_fsm_state_next <= READY;
 		when LINE_REQ =>
 			if rb_read_en = '0' then ringbuffer_fsm_state_next <= LINE_RDY;
 			end if;
 		when LINE_RDY =>
+			ringbuffer_fsm_state_next <= READY;
+		when PARS_REQ =>
+			if rb_pars_en = '0' then ringbuffer_fsm_state_next <= PARS_RDY;
+			end if;
+		when PARS_RDY =>
 			ringbuffer_fsm_state_next <= READY;
 		when NEW_LINE =>
 			ringbuffer_fsm_state_next <= READY;
@@ -96,16 +123,21 @@ begin
 		when READ_RAM =>
 			ringbuffer_fsm_state_next <= WAIT_RAM;
 		when WAIT_RAM =>
-			ringbuffer_fsm_state_next <= READY;
+			if lineCounter >= 80 then
+				ringbuffer_fsm_state_next <= LINE_REQ;
+			else
+				ringbuffer_fsm_state_next <= READ_RAM;
+			end if;
 		when others => null;
 	end case;
 end process next_state;
 
-output : process(ringbuffer_fsm_state, charPointer, byte_buffer, linePointer, rb_read_lineNr)
+output : process(ringbuffer_fsm_state, charPointer, byte_buffer, linePointer, rb_read_lineNr, lineCounter, lineRead)
 begin
 	linePointer_next <= linePointer;
 	charPointer_next <= charPointer;
 	ram_buffer_next <= ram_buffer;
+	lineCounter_next <= lineCounter;
 
 	rb_busy <= '1';
 	rb_read_data_rdy <= '0';
@@ -121,63 +153,63 @@ begin
 			linePointer_next <= 0;
 			charPointer_next <= 0;
 			rb_read_data <= (others => (others => '0'));
+			ram_buffer_next <= x"00";
+			lineCounter_next <= 0;
+			byte_Buffer_next <= x"00";
 		when READY =>
 			rb_busy <= '1';
 			rb_read_data_rdy <= '0';
-		when WRITE_CHAR =>
-			rb_busy <= '0';
-			if charPointer < LINE_LENGTH - 1 then
-				ram_buffer_next <= byte_buffer;
-				charPointer_next <= charPointer + 1;
-			end if;
 		when DELETE_CHAR =>
 			rb_busy <= '0';
 			if charPointer >= 0 then
-				--ram_next(linePointer, charPointer - 1) <= x"00";
-				--charPointer_next <= charPointer - 1;
+				wr <= '1';
+				data_in <= byte_buffer;
+				address <= charPointer-1+linePointer * 80;
+				charPointer_next <= charPointer - 1;
 			end if;
 		when LINE_REQ =>
 			rb_busy <= '0';
-			--if conv_integer((rb_read_lineNr+linePointer)) >= LINE_NUMB then
-			--	for i in 0 to LINE_LENGTH -1 loop
-			--		rb_read_data(i) <= ram(conv_integer((rb_read_lineNr+linePointer)) - LINE_NUMB, i);
-			--	end loop;
-			--else
-			--	for i in 0 to LINE_LENGTH -1 loop
-			--		rb_read_data(i) <= ram(conv_integer((rb_read_lineNr+linePointer)), i);
-			--	end loop;
-			--end if;
-			--rb_read_data_rdy <= '1';
+			rb_read_data_rdy <= '1';
+			rb_read_data <= lineBuffer;
 		when LINE_RDY =>
 			rb_busy <= '0';
-			--rb_read_data_rdy <= '1';
+			rb_read_data_rdy <= '1';
+			rb_read_data <= lineBuffer;
+		when PARS_REQ =>
+			rb_busy <= '0';
+			rb_pars_data_rdy <= '1';
+			rb_read_data <= lineBuffer;
+		when PARS_RDY =>
+			rb_busy <= '0';
+			rb_pars_data_rdy <= '1';
+			rb_read_data <= lineBuffer;
 		when NEW_LINE =>
 			rb_busy <= '0';
 			--Der Zeiger auf die Zeile wird um eins erhöht und auf Überlauf kontrolliert
+			--TODO Die neue Zeile muss gelöscht werden
 			if linePointer >= LINE_NUMB - 1 then
 				linePointer_next <= 0;
-				--Die neue Zeile wird gelöscht
-				--for i in 0 to LINE_LENGTH -1 loop
-				--	ram_next(0,i) <= x"00";		--TODO nur zum testen, später wieder x"00"
-				--end loop;		
 			else
-				--linePointer_next <= linePointer + 1;
-				--Die neue Zeile wird gelöscht
-				--for i in 0 to LINE_LENGTH -1 loop
-				--	ram_next(linePointer+1,i) <= x"00"; 	--TODO nur zum testen, später wieder x"00"
-				--end loop;
+				linePointer_next <= linePointer + 1;
 			end if;
 			--Der Char Zeiger wird auf 0 zurückgesetzt
 			charPointer_next <= 0;
 		when WRITE_RAM =>
-			wr <= '1';
-			data_in <= ram_buffer;
-			address <= charPointer+linePointer*80;
+			rb_busy <= '0';
+			if charPointer < LINE_LENGTH - 1 then
+				wr <= '1';
+				data_in <= byte_buffer;
+				address <= charPointer+linePointer * 80;
+				charPointer_next <= charPointer + 1;
+			end if;
 		when READ_RAM =>
-			address <= charPointer+linePointer*80;
+			rb_busy <= '0';
+			address <= lineRead*80 + lineCounter;
 		when WAIT_RAM =>
-			address <= charPointer;
-			ram_buffer <= data_out;
+			rb_busy <= '0';
+			address <= lineRead*80 + lineCounter;
+			lineBuffer_next(lineCounter) <= data_out;			
+			lineCounter_next <= lineCounter + 1;
 		when others => null;
 	end case;
 end process output;
