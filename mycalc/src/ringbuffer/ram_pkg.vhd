4058cfc4edd9336b87820db8cd65dc333253c6c1
LukasPetermann <LukasPetermann.hotmail.com>
2010-05-28 21:40:46 +0200

    kleiner Fehler im Package

--- mycalc/src/ringbuffer/ram_pkg.vhd
+++ mycalc/src/ringbuffer/ram_pkg.vhd
@@ -4,8 +4,8 @@ use ieee.numeric_std.all;
 
 use work.big_pkg.all;
 
-package ringbuffer2_pkg is
-    component ringbuffer2_ent IS
+package ram_pkg is
+    component ram_ent IS
 	port(
 		sys_clk		: in std_logic;	
 		sys_res_n	: in std_logic;
@@ -14,5 +14,5 @@ package ringbuffer2_pkg is
 		data_in		: in std_logic_vector(7 downto 0);
 		data_out	: out std_logic_vector(7 downto 0)
 	);
-    END component ringbuffer2_ent;
-end package ringbuffer2_pkg;
+    END component ram_ent;
+end package ram_pkg;
