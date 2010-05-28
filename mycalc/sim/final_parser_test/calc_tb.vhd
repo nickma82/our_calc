7c28704d0802ce045f4300ea4a3354330162ef11
Nick Ma <nick.ma@aon.at>
2010-05-28 09:33:55 +0200

    final parser TB

---
+++ mycalc/sim/final_parser_test/calc_tb.vhd
@@ -0,0 +1,143 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+--use work.alu_pkg.all;
+use work.big_pkg.all;
+use work.parser_pkg.all;
+
+entity parser_tb is
+	
+end entity parser_tb;
+
+architecture behav of parser_tb is
+  signal sys_clk, sys_res_n: std_logic;
+  signal stop : boolean := false;
+  	
+  	constant RESET_VALUE: std_logic := '0';
+	constant SIZE: INTEGER := 32;
+	
+
+	signal ps_start:	STD_LOGIC;
+	signal parse_new_data: 	STD_LOGIC;
+
+	signal parse_data:	RESULT_LINE; 	--Der neue ASCII Code.
+	signal parse_state:	parser_status_TYPE;	--
+	
+	signal rb_busy: STD_LOGIC;
+	signal rb_read_en:	STD_LOGIC;
+	signal rb_read_lineNr:	std_logic_vector(7 downto 0);
+	signal rb_read_data_rdy: STD_LOGIC;
+	signal rb_read_data:	 RAM_LINE;
+	
+	
+	component parser_top is
+	generic
+	(
+			RESET_VALUE : std_logic := '0'
+	);
+	port
+	(	sys_clk, sys_res_n       : in    std_logic;	
+			
+		ps_start:	IN STD_LOGIC;
+		parse_new_data: 	out STD_LOGIC;
+		
+		parse_data:	out ASCII_CHAR; --Der neue ASCII Code.
+		rb_busy:	in  STD_LOGIC;
+		rb_read_en:	out STD_LOGIC; 	--Eine neue Zeile wird angefordert.
+		rb_read_lineNr:	out std_logic_vector(7 downto 0);	--Die neue Zeile die gelesen werden soll.
+		rb_read_data_rdy:in STD_LOGIC;	--Die neue Zeile kann gelesen werden.
+		rb_read_data:	in  RAM_LINE
+		
+	);
+	end component parser_top;
+	
+	
+	signal intern_charUnit_on: std_logic := '0';
+	signal intern_charUnit_on_last: std_logic := '0';
+begin --behave
+  --uut : entity work.parser_top
+  uut: entity work.parser_top
+	generic map
+	(
+			RESET_VALUE => RESET_VALUE
+	)
+	port map
+	(	sys_clk => sys_clk,
+		sys_res_n => sys_res_n,
+			
+		ps_start	=> ps_start,
+		parse_new_data	=> parse_new_data,
+		parse_data	=> parse_data,
+		
+		rb_busy	=> rb_busy,
+		rb_read_en	=> rb_read_en,
+		rb_read_lineNr	=> rb_read_lineNr,
+		rb_read_data_rdy=> rb_read_data_rdy,
+		rb_read_data	=> rb_read_data
+	);
+    
+  process
+  begin
+    sys_clk <= '0';
+    wait for 15 ns;
+    sys_clk <= '1';
+    if stop = true then
+      wait;
+    end if;
+    wait for 15 ns;
+  end process;
+  
+  
+
+  
+  
+  process
+  begin
+    sys_res_n <= '0';
+    --######## RESET Pins #######
+	-- INIT
+	for i in 0 to 80 loop	
+		rb_read_data(i) <= x"00";
+	end loop;
+	--parser
+	rb_read_data_rdy<= '0';
+	ps_start <= '1';
+    	rb_read_data(0) <= x"31";
+	rb_read_data(1) <= x"32";
+	rb_read_data(2) <= x"2B";
+	rb_read_data(3) <= x"39";
+	rb_read_data(4) <= x"30";
+	rb_read_data(5) <= x"31";
+	rb_read_data(6) <= x"00";
+	--test input digitUnit
+	--##END RESET##
+	wait for 100 ns;
+    sys_res_n <= '1';
+    -- BEGIN TESTS
+    	
+    	rb_busy<= '1';
+	wait for 10 us;
+	
+	rb_read_data_rdy<='1';
+	wait for 5 us;
+	
+--     wait for 500 ns;
+--     calc_data <=  to_signed(-13, SIZEI); --Integer to Signed
+--     calc_data2 <= to_signed(11, SIZEI);
+--     calc_operator <= MULTIPLIKATION;
+--     wait for 10 ns;
+--     calc_start <= '1';
+--     wait for 2 us;
+--     -- coverage off
+--     assert calc_result= to_signed(-143, SIZEI)
+-- 		report "result failed" 	severity failure;
+--     -- coverage on
+--     calc_start <= '0';
+
+	
+	
+    wait for 200 us;
+    stop <= true;
+    wait;
+  end process;
+end architecture behav;
---
+++ mycalc/sim/final_parser_test/calc_tb_behav.do
@@ -0,0 +1,86 @@
+if {[file exists behav_work]} {
+  vdel -all -lib behav_work
+}
+
+# create work library directory
+vlib behav_work
+
+# map directory to library name "work"
+vmap work behav_work
+
+variable DIR ../../src
+
+#compile vhdl files
+vcom -work work $DIR/big_bug.vhd
+vcom -work work $DIR/libs/sync.vhd
+vcom -work work $DIR/libs/sync.vhd
+vcom -work work $DIR/libs/sync_beh.vhd
+vcom -work work $DIR/libs/sync_pkg.vhd
+vcom -work work $DIR/libs/math_pkg.vhd
+vcom -work work $DIR/debounce/debounce.vhd
+vcom -work work $DIR/debounce/debounce_fsm.vhd
+vcom -work work $DIR/debounce/debounce_fsm_beh.vhd
+vcom -work work $DIR/debounce/debounce_pkg.vhd
+vcom -work work $DIR/debounce/debounce_struct.vhd
+#vcom -work work $DIR/debounce/debounce_top.vhd
+vcom -work work $DIR/libs/counter.vhd
+vcom -work work $DIR/libs/counter_beh.vhd
+
+
+#####ALU
+vcom -work work $DIR/alu/alu_div_ent.vhd
+vcom -work work $DIR/alu/alu_div_arc.vhd
+vcom -work work $DIR/alu/alu_fsm_ent.vhd
+vcom -work work $DIR/alu/alu_fsm_arc.vhd
+vcom -work work $DIR/alu/alu_top.vhd
+vcom -work work $DIR/alu/alu_top_struct.vhd
+vcom -work work $DIR/alu/alu_pkg.vhd
+
+###PARSER
+vcom -work work $DIR/parser/parser_pkg.vhd
+vcom -work work $DIR/parser/parser_sm_ent.vhd
+vcom -work work $DIR/parser/parser_sm_arc.vhd
+###vcom -work work $DIR/parser/char_unit_ent.vhd
+###vcom -work work $DIR/parser/char_unit_arc.vhd
+vcom -work work $DIR/parser/b2bcd_ent.vhd
+vcom -work work $DIR/parser/b2bcd_arc.vhd
+vcom -work work $DIR/parser/char_unit_ent.vhd
+vcom -work work $DIR/parser/char_unit_arc.vhd
+vcom -work work $DIR/parser/parser_top.vhd
+vcom -work work $DIR/parser/parser_top_struct.vhd
+#vcom -work work $DIR/parser/.vhd
+
+
+## compile testbench
+vcom -work work calc_tb.vhd
+
+
+
+## start simulation
+#coverage zeigt %des Codes der Simuliert ist
+# EIN START SIM cmd
+#vsim -coverage work.conf
+vsim -voptargs=+acc work.parser_tb
+
+view -undock wave
+
+# add signals to waveform
+# add all testbench signals
+add wave -radix Decimal *
+
+# add internal signals of unit under test
+#add wave -divider alu
+#add wave -group "Div Comm" uut/div_*
+add wave -group "alu" -radix Decimal uut/alu_top_inst/*
+add wave -group "parser" -radix Decimal uut/parser_sm_inst/*
+add wave -group "char_unit" -radix Decimal uut/char_unit_inst/*
+add wave -group "b2bcd" -radix Decimal uut/b2bcd_ent_inst/*
+
+
+# auto-run simulation
+#run 2 ms
+run -all
+wave zoomfull
+
+#restart -f
+#run -all
